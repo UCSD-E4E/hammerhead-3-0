f1ebb70ed2f35f90acf3f497741630b14e2a1253
awilby <antonellawilby@gmail.com>
2013-11-13 13:03:45 -0800

    controls demo node for keeping stable distance from target

---
+++ demo/src/controls_demo/src/controls_demo.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+import roslib; roslib.load_manifest('controls_demo')
+import ropsy
+from std_msgs.msg import Float32
+import sys
+
+###
+### Author: Antonella Wilby
+### Email: awilby@ucsd.edu
+###
+### Gets distance from target from target tracker node and keeps Stingray
+### at stable distance from target.
+###
+
+### TODO
+### Implement me!
+
+class DemoController:
+    def __init__(self):
+        """Initializes DemoController."""
+        pass
+    
+
+
+def main(args):
+    """..."""
+    pass
+
+
+# Calls main and passes system arguments if run standalone
+if __name__ == "__main__":
+    main(sys.argv)
